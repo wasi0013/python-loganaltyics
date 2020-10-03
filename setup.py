100545fba9b4ec6c4ea95e84123486fdc3de8bd3
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2020-10-03 09:42:25 +0600

    updated setup.py to support python 3.6 and above only

--- setup.py
+++ setup.py
@@ -19,14 +19,12 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Wasi Mohammed Abdullah",
     author_email='wasi0013@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
